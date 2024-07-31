5e853cbbe73373dbb86481e1bb401f0db98d772b
Ragda Alakhras <Ragda.alakhras@seera.sa>
2024-07-31 13:52:37 +0300

    EMS-697

--- src/controllers/script-controller.js
+++ src/controllers/script-controller.js
@@ -2,7 +2,6 @@ const Boom = require('boom');
 const HttpStatus = require('http-status-codes/index');
 const util = require('util');
 const vm = require('vm');
-const fs = require('fs');
 
 
 /**
@@ -13,22 +12,14 @@ const fs = require('fs');
 const executeOne = async (request, reply) => {
 
     try {
-        var contextVaribales = { util, fs };
+        var contextVaribales = { util};
         contextVaribales = mergeRequestData(contextVaribales, request);
-        console.log(contextVaribales); // 42
-// Define the context with Node.js built-in modules
+        console.log(contextVaribales);
         vm.createContext(contextVaribales);
-        // const x = 1;
-        // const context = { x: 2 };
-        // vm.createContext(context); ==> here context will be global variables
-        // vm.createContext(context); // Contextify the object.
-
-        const code = 'x += 40; var y = 17;';
-        console.log("XXXZZZZZ"); // 42
         vm.runInContext(request.body.inputScript, contextVaribales);
 
-        const extractedMap = new Map();
 
+        const extractedMap = new Map();
         let responseVariables = request.body.response;
         responseVariables.forEach(key => {
             if (key in contextVaribales) {
@@ -37,36 +28,12 @@ const executeOne = async (request, reply) => {
         });
 
         const resultObject = mapToObject(extractedMap);
-
-        console.log("lklklklklk"); // 42
-        console.log(extractedMap); // 42
-        console.log(resultObject); // 42
-
-        // console.log(x); // 1; y is not defined.
-        // const contextobj = { count: 8 }
-        // // let consoleOutput = '';
-        // //
-        // // // Redirect console.log to capture output
-        // // contextobj.console = {
-        // //     log: (...args) => {
-        // //         consoleOutput += args;
-        // //     }
-        // // };
-        // console.log("cccc");
-        // vm.createContext(contextobj);
-        // console.log("ddddd");
-        // vm.runInContext(request.body.inputScript, contextobj);
-        // console.log("mmmmm");
-        // // contextobj.consoleOutput = consoleOutput;
-        // console.log("The output is: ", contextobj.consoleOutput);
-        // console.log("The output is: ", contextobj);
-        console.log("vvvvvv");
+        console.debug(resultObject);
         reply.code(HttpStatus.OK)
             .header('Content-Type', 'application/json')
             .send(JSON.stringify(resultObject));
     } catch  (e) {
-        console.log("xxxxxx");
-        console.error(e.message);
+        console.debug(e.message);
         return Boom.boomify(e);
     }
 };
