f75b9c84259e2ba3f0e0e1c18ee4ed7c51be78fd
Ragda Alakhras <Ragda.alakhras@seera.sa>
2024-10-23 14:07:41 +0300

    EMS-783 Fix error message status

--- src/app.js
+++ src/app.js
@@ -34,7 +34,7 @@ module.exports = fastify;
         fastify.register(v1RoutesMiddleware, { prefix: '/v1' });
 
         // Server
-        await fastify.listen(config.get('API_HTTP_PORT', 80), '0.0.0.0');
+        await fastify.listen(config.get('NODE_PORT', 6661), '0.0.0.0');
         fastify.log.info(
             '%s listening in %s environment',
             config.name,
